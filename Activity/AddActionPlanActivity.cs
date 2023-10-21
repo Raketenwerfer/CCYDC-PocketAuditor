dd95a1acda8b742d7b2149464225a7dfde13976d
crazylittleS <christinesarno230@gmail.com>
2023-10-21 15:25:44 +0800

    gegeggegegge

--- Activity/AddActionPlanActivity.cs
+++ Activity/AddActionPlanActivity.cs
@@ -15,7 +15,8 @@ namespace PocketAuditor.Activity
     [Activity(Label = "AddActionPlanActivity")]
     public class AddActionPlanActivity : AppCompatActivity
     {
-        Button cancelPlan;
+        EditText planName, planCateName, planCateDesc, planPasteLink;
+        Button addPlan, cancelPlan;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -24,9 +25,30 @@ namespace PocketAuditor.Activity
             // Create your application here
             SetContentView(Resource.Layout.add_action_plans);
 
-            cancelPlan = FindViewById<Button>(Resource.Id.cancelPlanBtn);
+            planName = FindViewById<EditText>(Resource.Id.planName);
+            planCateName = FindViewById<EditText>(Resource.Id.plan_CatName);
+            planCateDesc = FindViewById<EditText>(Resource.Id.plan_CatDescription);
+            planPasteLink = FindViewById<EditText>(Resource.Id.plan_PasteLink);
 
+            addPlan = FindViewById<Button>(Resource.Id.addPlanBtn);
+            cancelPlan = FindViewById<Button>(Resource.Id.cancelPlanBtn);
+             
+            addPlan.Click += AddPlan_Click;
             cancelPlan.Click += CancelPlan_Click; 
+        }
+
+        private void AddPlan_Click(object sender, EventArgs e)
+        {
+            string Name = planName.Text; 
+            string cateName = planCateName.Text;
+            string cateDisc = planCateDesc.Text;
+            string pasteLink = planPasteLink.Text;
+
+            if (string.IsNullOrWhiteSpace(Name) || string.IsNullOrWhiteSpace(cateName) ||
+                string.IsNullOrWhiteSpace(cateDisc) || string.IsNullOrWhiteSpace(pasteLink))
+            {
+                Toast.MakeText(this, "Fields must be Filled", ToastLength.Short).Show();
+            }
         }
 
         private void CancelPlan_Click(object sender, EventArgs e)
